20befc9e7a18589e0ae60c390efe8daf303e7b16
JoshuaGal2020 <gg0210032019@unab.edu.sv>
2021-10-02 18:02:31 -0600

    First update

---
+++ ejercicios/app/src/main/java/com/example/ejercicios/Empleado.kt
@@ -0,0 +1,9 @@
+package com.example.ejercicios
+
+class Empleado (nombre:String,val Sueldo:Int, Area:String, Cargo:String,AnhosTrabajo:Int){
+
+
+    var edad = 0
+    get()= field
+
+}
---
+++ ejercicios/app/src/main/java/com/example/ejercicios/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.ejercicios
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //operario("Joshua",20,12,1)
+        //tabla(2,10)
+    }
+
+    fun operario(nombre:String,pagohora:Int,HorasTrabajadas:Int,horasextras:Int){
+        println("El nombre del operario es $nombre")
+        var sueldo = HorasTrabajadas*pagohora
+        var pagoextra = horasextras*pagohora*2
+
+        var pagototal = sueldo+pagoextra
+        println("El operario: $nombre recibira un sueldo base de $sueldo por el trabajo realizado ademas se le agregaran el sueldo por horas extras realizadas que es $pagoextra Su sueldo total devengado en este mes sera de $pagototal" )
+
+    }
+    fun Tabla(tabla:Int,final:Int?= null){
+        var termino = 10
+        if(final!=null){
+            termino= final
+        }
+        for (num in 1..termino){
+            var resultado = num*tabla
+            println("$tabla * $num = $resultado ")
+        }
+    }
+    fun empleado(){
+        var empleado1 = Empleado("joshua", 450,"lavanderia","Administrador",2)
+
+    }
+}
